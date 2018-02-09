4c6793fc6965540ccd493c398e4e5dacdd647752
Alexis Ramos <alexisramoscaballero@gmail.com>
2018-02-09 17:46:32 -0600

    Se actualizo la politica para detectar tarjetas o equipos

--- Politicas/Politicas Actualizadas y con comentarios/NetcoolDTCMDB.java
+++ Politicas/Politicas Actualizadas y con comentarios/NetcoolDTCMDB.java
@@ -1,115 +1,247 @@
-/**POLITICA PARA ENRIQUECIMIENTO EN LA CMDB**/
-
-/*Propósito: Proceso de enriquecimiento en la CMDB 
-  Fuentes: OMNIbus   DataSource: defaultobjectserver  DataType=data
-
-  Gestores involucrados:   
-  Fecha de Creación: 25/01/2018
-  Fecha de Actualización: 25/01/2018 */
-
-log("NetcoolDTCMDB");
+log("NetcoolDTCMDB_");
 
 clli = @TMX_NodeName;
 serial = @Serial;
+//serial = 90331497;
 identificador = @Identifier;
 usr=0;
 fch=getdate();
-repisa = @TMX_ShelfNumber;
-puerto = @PhysicalPort;
-slot = @PhysicalSlot;
-server = @ServerName;
 Source="defaultobjectserver";
+repisa = "0" + @TMX_ShelfNumber;
+puerto = "0" + @PhysicalPort;
+slot = "0" +  @PhysicalSlot;
+log("\n"+"CLLI ES: "+ @TMX_NodeName+"\n" + "El SERIAL ES:"+@Serial+"\nLa repisa es: "+@TMX_ShelfNumber+" \nEl puerto es: "+ @PhysicalPort + "\nEl slot es: "+ @PhysicalSlot +"\nEl servidor es: "+ @ServerName);
 
-nodo = SUBSTRING(@TMX_NodeName, 0, 11);
 
-log("\n"+"CLLI ES: "+ @TMX_NodeName+"\n" + "El SERIAL ES:"+@Serial+"\n" + "\nLa repisa es: " +repisa +"\El puerto es: " + puerto + "\nEl slot es: " + slot +"\El primer indice del nodo es: " + nodo);
+//comprobamos el clli si es de equipo o tarjeta
+
+if (Length(clli) == 11) {
+    log("Iniciar política 'NetcoolDTCMDB_'...");
+    //Especifique el nombre de paquete tal como se ha definido al compilar WSDL en Impact
+    WSSetDefaultPKGName('NetcoolDTCMDB');
+
+    //Especificar parámetros
+    RetrieveNetcoolDTCMDBRequestDocument=WSNewObject("com.hp.schemas.sm._7.RetrieveNetcoolDTCMDBRequestDocument");
+    _RetrieveNetcoolDTCMDBRequest=WSNewSubObject(RetrieveNetcoolDTCMDBRequestDocument,"RetrieveNetcoolDTCMDBRequest");
 
 
-//Validacion para los servidores
-if (server == "CNSAOTD"){
+    _Model = WSNewSubObject(_RetrieveNetcoolDTCMDBRequest,"Model");
 
-}
+    _Keys = WSNewSubObject(_Model,"Keys");
+    _Keys['Query'] = 'display.name="'+clli+'"';
 
-    //Empieza ejecucion del ws para obtener el enriquecimiento de la CMDB
-log("EMPIEZA LA EJECUCION DEL WS, capurando los datos");
-WSSetDefaultPKGName('SM_DTCMDB');
+    _CIID = WSNewSubObject(_Keys,"CIID");
 
-RetrieveNetcoolDTCMDBRequestDocument=WSNewObject("com.hp.schemas.sm._7.RetrieveNetcoolDTCMDBRequestDocument");
-_RetrieveNetcoolDTCMDBRequest=WSNewSubObject(RetrieveNetcoolDTCMDBRequestDocument,"RetrieveNetcoolDTCMDBRequest");
+    _Instance = WSNewSubObject(_Model,"Instance");
 
-_Model = WSNewSubObject(_RetrieveNetcoolDTCMDBRequest,"Model");
+    _FileDevice = WSNewSubObject(_Instance,"FileDevice");
 
-_Keys = WSNewSubObject(_Model,"Keys");
-    //_Keys['Query'] = 'display.name="'+nodo+'" + || + "'+repisa+'" + || + "'+puerto+'" + || + "'+slot'"'; //Agregar la variable del nodo
-_Keys['Query'] = 'display.name="'+nodo+'"+"/"+"'+repisa+'"+"/"+"'+puerto+'"+"/"+"'+slot+'"';
-_Instance = WSNewSubObject(_Model,"Instance");
+    _CIID = WSNewSubObject(_FileDevice,"CIID");
 
-WSParams = {RetrieveNetcoolDTCMDBRequestDocument};
 
-  //Especifique un nombre de servicio web, un punto final y un método
-WSService = 'NetcoolDTCMDB';
-WSEndPoint = 'http://qcalswpr:13086/SM/7/ws';
-WSMethod = 'RetrieveNetcoolDTCMDB';
+    WSParams = {RetrieveNetcoolDTCMDBRequestDocument};
+
+   log("11 DIGITOS: " + WSParams);
+
+
+    //Especifique un nombre de servicio web, un punto final y un método
+    WSService = 'NetcoolDTCMDB';
+    WSEndPoint = 'http://qcalswpr:13083/SM/7/ws';
+    WSMethod = 'RetrieveNetcoolDTCMDB';
 
     //Habilitar seguridad de servicio web
-callProps = NewObject();
-callProps.Username="netcooldt";
-callProps.Password="netcooldt";
-callProps.ReuseHttpClient = false;
-callProps.KeepAlive = false;
-callProps.CloseConnection=true;
+    callProps = NewObject();
+    callProps.Username="netcooldt";
+    callProps.Password="netcooldt";
+    callProps.ReuseHttpClient = false;
+    callProps.KeepAlive = false;
+    callProps.CloseConnection=true;
 
-log("Se va a invocar la llamada de servicio web RetrieveNetcoolDTCMDB ......");
+    log("Se va a invocar la llamada de servicio web RetrieveNetcoolDTCMDB ......");
 
-WSInvokeDLResult = WSInvokeDL(WSService, WSEndPoint, WSMethod, WSParams, callProps);
-log("Resultado devuelto de la llamada de servicio web RetrieveNetcoolDTCMDB: " +WSInvokeDLResult);
+    WSInvokeDLResult = WSInvokeDL(WSService, WSEndPoint, WSMethod, WSParams, callProps);
+    log("Resultado devuelto de la llamada de servicio web RetrieveNetcoolDTCMDB: " +WSInvokeDLResult);
+    log("TERMINA LA LLAMADA DEL WS");
 
+    log("Fin de la politica NetcoolDTCMDB_A");
+    
     //Se extrae del resultado del ws en WSInvokeDLResult los valores CIID, estatus, subred, tipoRed
-ciid = RExtract(WSInvokeDLResult,".*<CIID type=.*>(.*)</CIID>.*"); //de esta forma se obtiene el campo de la respuesta del webservuce
-estatus = RExtract(WSInvokeDLResult,".*<estatus type=.*>(.*)</estatus>.*");
-subRed = RExtract(WSInvokeDLResult,".*<subred type=.*>(.*)</subred>.*");
-tipoRed = RExtract(WSInvokeDLResult,".*<tipoRed type=.*>(.*)</tipoRed>.*");
-modelo = RExtract(WSInvokeDLResult,".*<modelo type=.*>(.*)</modelo>.*");
-elemetRed = RExtract(WSInvokeDLResult,".*<elementoRed type=.*>(.*)</elementoRed>.*");
-returnCode = RExtract(WSInvokeDLResult,'.*returnCode="([0-9]).*'); 
+    CIId= RExtract(WSInvokeDLResult,".*<CIID type=.*>(.*)</CIID>.*"); //de esta forma se obtiene el campo de la respuesta del webservuce
+    estatus = RExtract(WSInvokeDLResult,".*<estatus type=.*>(.*)</estatus>.*");
+    subRed = RExtract(WSInvokeDLResult,".*<subred type=.*>(.*)</subred>.*");
+    tipoRed = RExtract(WSInvokeDLResult,".*<tipoRed type=.*>(.*)</tipoRed>.*");
+    modelo = RExtract(WSInvokeDLResult,".*<modelo type=.*>(.*)</modelo>.*");
+    elemetRed = RExtract(WSInvokeDLResult,".*<elementoRed type=.*>(.*)</elementoRed>.*");
+    returnCode = RExtract(WSInvokeDLResult,'.*returnCode="([0-9]).*'); 
+    log("CIID: "+CIId+"\n"+"ESTATUS: "+estatus+"\n"+"SubRed: "+subRed+"\n"+"TIPO RED: "+tipoRed+"\n"+"EL MODELO ES: "+ modelo+"\n"+"EL ELEMENTO DE RED ES: "+elemetRed);
 
-log("CIID: "+ciid+"\n"+"ESTATUS: "+estatus+"\n"+"SubRed: "+subRed+"\n"+"TIPO RED: "+tipoRed+"\n"+"EL MODELO ES: "+ modelo+"\n"+"EL ELEMENTO DE RED ES: "+elemetRed);
+    //Validaciones
+    if(CIId = NULL){CIId = "";}
+    if(estatus = NULL){estatus = "";}
+    if(subRed = NULL){subRed = "";}
+    if(tipoRed = NULL){tipoRed = "";}
+    if(modelo = NULL){modelo = "";}
+    if(elemetRed = NULL){elemetRed = "";}
 
-/***Se realizan las validaciones***/
-/**Se valida que los elemntos de res sean los esperados al igual que su estatus */
-if ((elementRed = "Equipo" && estatus = "GESTIONADO") || (elementRed = "Enlaces" && estatus = "GESTIONADO") || (elementRed = "Servicio" && estatus = "LIBERADO") || (elementRed = "Servicio Cliente Final" && estatus = "En operacion") || (elementRed = "Puerto" && estatus = "OCUPADO") || (elementRed = "Tarjeta" && estatus = "GESTIONADO")){
-    /**Para los elementos de red tipo equipo y que sean gestionados  */
-    if (elementoRed "Equipo" && estatus = "GESTIONADO"){
-        if (ciid != "" && modelo != "" && elementRed != "" && estatus != "" && subRed != "" && tipoRed != ""){
-            Filter1="Serial="+serial; //Este es el serial del evento
+    if ((CIId != "" && elemetRed != "" && estatus != "")|| (elementRed = "Equipo" && estatus = "GESTIONADO") || (elementRed = "Enlaces" && estatus = "GESTIONADO") || (elementRed = "Servicio" && estatus = "LIBERADO") || (elementRed = "Servicio Cliente Final" && estatus = "En operacion") || (elementRed = "Puerto" && estatus = "OCUPADO") || (elementRed = "Tarjeta" && estatus = "GESTIONADO")) 
+    {
+        Filter1="Serial="+serial; //Este es el serial del evento
+        log ("El serial del evento es: " +Filter1);
+        UpdateExpression="CMDB_Logical_Name='"+CIId+"',CMDB_Istatus='"+estatus+"',CMDB_Subred='"+subRed+"',CMDB_Tipo_Red='"+tipoRed+"',CMDB_Model='"+modelo+"',CMDB_Topologia='"+elemetRed+"',TMX_Promote = 26, ImpactFlag=200";
+        log("VALOR DE TMX_PROMOTE ANTES DE ACTUALIZAR: " + @TMX_Promote);
+        log ("El UPDATE EXPRESSION ES: "+UpdateExpression);
+        BatchUpdate('data', Filter1, UpdateExpression); //BatchUpdate sirve para actualizar el campo en el evento
+
+        //ACTUALIZACION EN BITACORA
+        msj= fch +"|" + "Evento: " + serial + "|" + "Enriquece evento" + "|" + "El CLLI se encontro en Service Manager";
+        MyKey = serial +":"+ usr +":"+ fch;
+        MySQL = "insert into alerts.journal (KeyField,Serial,UID,Chrono,Text1) values('"+ MyKey +"',"+ serial +","+ usr +","+ fch + ",'"+msj+"')";
+        log ("El Query del Insert en alerts.journal es: "+MySQL);
+        DirectSQL(Source,MySQL,false);
+    } elseif ((CIId = "" && elemetRed = "" && estatus = "")|| (elementRed = "Equipo" && estatus != "GESTIONADO") || (elementRed = "Enlaces" && estatus != "GESTIONADO") || (elementRed = "Servicio" && estatus != "LIBERADO") || (elementRed = "Servicio Cliente Final" && estatus != "En operacion") || (elementRed = "Puerto" && estatus != "OCUPADO") || (elementRed = "Tarjeta" && estatus != "GESTIONADO"))
+    {
+        if (returnCode == 9) {
+            //Se actualiza en la tabla alerts.status
+            Filter1="Serial="+serial;
             log ("El serial del evento es: " +Filter1);
-            UpdateExpression="CMDB_Logical_Name='"+ciid+"',CMDB_Istatus='"+estatus+"',CMDB_Subred='"+subRed+"',CMDB_Tipo_Red='"+tipoRed+"',CMDB_Model='"+modelo+"',CMDB_Topologia='"+elemetRed+"',TMX_Promote = 26, ImpactFlag = 200";
-            log("VALOR DE TMX_PROMOTE ANTES DE ACTUALIZAR: " + @TMX_Promote);
+            UpdateExpression="ImpactFlag=201, TMX_Promote = 0";
             log ("El UPDATE EXPRESSION ES: "+UpdateExpression);
-            BatchUpdate('data', Filter1, UpdateExpression); //BatchUpdate sirve para actualizar el campo en el evento
+            BatchUpdate('data', Filter1, UpdateExpression);
 
-            //ACTUALIZACION EN BITACORA
-            msj= fch +"|" + "Evento: " + serial + "|" + "Enriquece evento" + "|" + "El CLLI se encontro en Service Manager";
-            MyKey = serial +":"+ usr +":"+ fch; 
+            //Se actualiza en la tabla alerts.jorunal
+            msj= fch +"|" + "Evento: " + serial + "|" + "No se encontro informacion" + "|" + "Sin informacion en la fuente de enriquecimiento";
+            MyKey = serial +":"+ usr +":"+ fch;
             MySQL = "insert into alerts.journal (KeyField,Serial,UID,Chrono,Text1) values('"+ MyKey +"',"+ serial +","+ usr +","+ fch + ",'"+msj+"')";
-            log ("El Query del Insert en alerts.journal es: "+MySQL);
             DirectSQL(Source,MySQL,false);
-        }else{
-            Filter1="Serial="+serial; //Este es el serial del evento
+            log ("El Query del Insert en alerts.journal es: "+MySQL);
+        }elseif(returnCode == 0){
+            Filter1="Serial="+serial;
             log ("El serial del evento es: " +Filter1);
-            UpdateExpression="CMDB_Logical_Name='"+ciid+"',CMDB_Istatus='"+estatus+"',CMDB_Subred='"+subRed+"',CMDB_Tipo_Red='"+tipoRed+"',CMDB_Model='"+modelo+"',CMDB_Topologia='"+elemetRed+"',TMX_Promote = 0, ImpactFlag = 201";
-            log("VALOR DE TMX_PROMOTE ANTES DE ACTUALIZAR: " + @TMX_Promote);
+            UpdateExpression="ImpactFlag=201, TMX_Promote = 0,CMDB_Logical_Name='"+CIId+"',CMDB_Istatus='"+estatus+"',CMDB_Subred='"+subRed+"',CMDB_Tipo_Red='"+tipoRed+"',CMDB_Model='"+modelo+"',CMDB_Topologia='"+elemetRed+"'";
             log ("El UPDATE EXPRESSION ES: "+UpdateExpression);
-            BatchUpdate('data', Filter1, UpdateExpression); //BatchUpdate sirve para actualizar el campo en el evento
+            BatchUpdate('data', Filter1, UpdateExpression);
 
-            //ACTUALIZACION EN BITACORA
+
             msj= fch +"|" + "Evento: " + serial + "|" + "Enriquece evento" + "|" + "Informacion incompleta en la fuente de enriquecimiento";
-            MyKey = serial +":"+ usr +":"+ fch; 
+            MyKey = serial +":"+ usr +":"+ fch;
             MySQL = "insert into alerts.journal (KeyField,Serial,UID,Chrono,Text1) values('"+ MyKey +"',"+ serial +","+ usr +","+ fch + ",'"+msj+"')";
             log ("El Query del Insert en alerts.journal es: "+MySQL);
             DirectSQL(Source,MySQL,false);
         }
     }
-    
+} else{
+    log("Iniciar política 'NetcoolDTCMDB_'...");
+    //Especifique el nombre de paquete tal como se ha definido al compilar WSDL en Impact
+    WSSetDefaultPKGName('NetcoolDTCMDB');
 
+    //Especificar parámetros
+    RetrieveNetcoolDTCMDBRequestDocument=WSNewObject("com.hp.schemas.sm._7.RetrieveNetcoolDTCMDBRequestDocument");
+    _RetrieveNetcoolDTCMDBRequest=WSNewSubObject(RetrieveNetcoolDTCMDBRequestDocument,"RetrieveNetcoolDTCMDBRequest");
+
+
+    _Model = WSNewSubObject(_RetrieveNetcoolDTCMDBRequest,"Model");
+
+    _Keys = WSNewSubObject(_Model,"Keys");
+    _Keys['Query'] = 'display.name= "'+clli+''+'/'+''+repisa+''+'/'+slot+''+'/'+puerto+'"';
+
+    _CIID = WSNewSubObject(_Keys,"CIID");
+
+    _Instance = WSNewSubObject(_Model,"Instance");
+
+    _FileDevice = WSNewSubObject(_Instance,"FileDevice");
+
+    _CIID = WSNewSubObject(_FileDevice,"CIID");
+
+
+    WSParams = {RetrieveNetcoolDTCMDBRequestDocument};
+
+    log("MAS DE 11 DIGITOS: " + WSParams);
+
+
+
+    //Especifique un nombre de servicio web, un punto final y un método
+    WSService = 'NetcoolDTCMDB';
+    WSEndPoint = 'http://qcalswpr:13083/SM/7/ws';
+    WSMethod = 'RetrieveNetcoolDTCMDB';
+
+    //Habilitar seguridad de servicio web
+    callProps = NewObject();
+    callProps.Username="netcooldt";
+    callProps.Password="netcooldt";
+    callProps.ReuseHttpClient = false;
+    callProps.KeepAlive = false;
+    callProps.CloseConnection=true;
+
+    log("Se va a invocar la llamada de servicio web RetrieveNetcoolDTCMDB ......");
+
+    WSInvokeDLResult = WSInvokeDL(WSService, WSEndPoint, WSMethod, WSParams, callProps);
+    log("Resultado devuelto de la llamada de servicio web RetrieveNetcoolDTCMDB: " +WSInvokeDLResult);
+    log("TERMINA LA LLAMADA DEL WS");
+
+    log("Fin de la politica NetcoolDTCMDB_A");
+
+    //Se extrae del resultado del ws en WSInvokeDLResult los valores CIID, estatus, subred, tipoRed
+    CIId= RExtract(WSInvokeDLResult,".*<CIID type=.*>(.*)</CIID>.*"); //de esta forma se obtiene el campo de la respuesta del webservuce
+    estatus = RExtract(WSInvokeDLResult,".*<estatus type=.*>(.*)</estatus>.*");
+    subRed = RExtract(WSInvokeDLResult,".*<subred type=.*>(.*)</subred>.*");
+    tipoRed = RExtract(WSInvokeDLResult,".*<tipoRed type=.*>(.*)</tipoRed>.*");
+    modelo = RExtract(WSInvokeDLResult,".*<modelo type=.*>(.*)</modelo>.*");
+    elemetRed = RExtract(WSInvokeDLResult,".*<elementoRed type=.*>(.*)</elementoRed>.*");
+    returnCode = RExtract(WSInvokeDLResult,'.*returnCode="([0-9]).*'); 
+    log("CIID: "+CIId+"\n"+"ESTATUS: "+estatus+"\n"+"SubRed: "+subRed+"\n"+"TIPO RED: "+tipoRed+"\n"+"EL MODELO ES: "+ modelo+"\n"+"EL ELEMENTO DE RED ES: "+elemetRed);
+
+    //Validaciones
+    if(CIId = NULL){CIId = "";}
+    if(estatus = NULL){estatus = "";}
+    if(subRed = NULL){subRed = "";}
+    if(tipoRed = NULL){tipoRed = "";}
+    if(modelo = NULL){modelo = "";}
+    if(elemetRed = NULL){elemetRed = "";}
+
+    if ((CIId != "" && elemetRed != "" && estatus != "")|| (elementRed = "Equipo" && estatus = "GESTIONADO") || (elementRed = "Enlaces" && estatus = "GESTIONADO") || (elementRed = "Servicio" && estatus = "LIBERADO") || (elementRed = "Servicio Cliente Final" && estatus = "En operacion") || (elementRed = "Puerto" && estatus = "OCUPADO") || (elementRed = "Tarjeta" && estatus = "GESTIONADO")) 
+    {
+        Filter1="Serial="+serial; //Este es el serial del evento
+        log ("El serial del evento es: " +Filter1);
+        UpdateExpression="CMDB_Logical_Name='"+CIId+"',CMDB_Istatus='"+estatus+"',CMDB_Subred='"+subRed+"',CMDB_Tipo_Red='"+tipoRed+"',CMDB_Model='"+modelo+"',CMDB_Topologia='"+elemetRed+"',TMX_Promote = 26, ImpactFlag=200";
+        log("VALOR DE TMX_PROMOTE ANTES DE ACTUALIZAR: " + @TMX_Promote);
+        log ("El UPDATE EXPRESSION ES: "+UpdateExpression);
+        BatchUpdate('data', Filter1, UpdateExpression); //BatchUpdate sirve para actualizar el campo en el evento
+
+        //ACTUALIZACION EN BITACORA
+        msj= fch +"|" + "Evento: " + serial + "|" + "Enriquece evento" + "|" + "El CLLI se encontro en Service Manager";
+        MyKey = serial +":"+ usr +":"+ fch;
+        MySQL = "insert into alerts.journal (KeyField,Serial,UID,Chrono,Text1) values('"+ MyKey +"',"+ serial +","+ usr +","+ fch + ",'"+msj+"')";
+        log ("El Query del Insert en alerts.journal es: "+MySQL);
+        DirectSQL(Source,MySQL,false);
+    } elseif ((CIId = "" && elemetRed = "" && estatus = "")|| (elementRed = "Equipo" && estatus != "GESTIONADO") || (elementRed = "Enlaces" && estatus != "GESTIONADO") || (elementRed = "Servicio" && estatus != "LIBERADO") || (elementRed = "Servicio Cliente Final" && estatus != "En operacion") || (elementRed = "Puerto" && estatus != "OCUPADO") || (elementRed = "Tarjeta" && estatus != "GESTIONADO"))
+    {
+        if (returnCode == 9) {
+            //Se actualiza en la tabla alerts.status
+            Filter1="Serial="+serial;
+            log ("El serial del evento es: " +Filter1);
+            UpdateExpression="ImpactFlag=201, TMX_Promote = 0";
+            log ("El UPDATE EXPRESSION ES: "+UpdateExpression);
+            BatchUpdate('data', Filter1, UpdateExpression);
+
+            //Se actualiza en la tabla alerts.jorunal
+            msj= fch +"|" + "Evento: " + serial + "|" + "No se encontro informacion" + "|" + "Sin informacion en la fuente de enriquecimiento";
+            MyKey = serial +":"+ usr +":"+ fch;
+            MySQL = "insert into alerts.journal (KeyField,Serial,UID,Chrono,Text1) values('"+ MyKey +"',"+ serial +","+ usr +","+ fch + ",'"+msj+"')";
+            DirectSQL(Source,MySQL,false);
+            log ("El Query del Insert en alerts.journal es: "+MySQL);
+        }elseif(returnCode == 0){
+            Filter1="Serial="+serial;
+            log ("El serial del evento es: " +Filter1);
+            UpdateExpression="ImpactFlag=201, TMX_Promote = 0,CMDB_Logical_Name='"+CIId+"',CMDB_Istatus='"+estatus+"',CMDB_Subred='"+subRed+"',CMDB_Tipo_Red='"+tipoRed+"',CMDB_Model='"+modelo+"',CMDB_Topologia='"+elemetRed+"'";
+            log ("El UPDATE EXPRESSION ES: "+UpdateExpression);
+            BatchUpdate('data', Filter1, UpdateExpression);
+
+
+            msj= fch +"|" + "Evento: " + serial + "|" + "Enriquece evento" + "|" + "Informacion incompleta en la fuente de enriquecimiento";
+            MyKey = serial +":"+ usr +":"+ fch;
+            MySQL = "insert into alerts.journal (KeyField,Serial,UID,Chrono,Text1) values('"+ MyKey +"',"+ serial +","+ usr +","+ fch + ",'"+msj+"')";
+            log ("El Query del Insert en alerts.journal es: "+MySQL);
+            DirectSQL(Source,MySQL,false);
+        }
+    }
 }
