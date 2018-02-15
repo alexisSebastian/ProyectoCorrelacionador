4475e78ff20989f592f7f0c7b58e91e3a09e5a4b
Alexis Ramos <alexisramoscaballero@gmail.com>
2018-02-14 19:04:42 -0600

    Se agrega la politica para eliminar eventos

---
+++ Politicas/NetcoolEventDelete.java
@@ -0,0 +1,14 @@
+log("NetcoolEventDelete");
+
+serial = @Serial;
+//serial = 90924759;
+
+
+
+log("Inicio de la politica NetcoolEventDelete");
+
+  Filter = "Serial="+serial;
+  BatchDelete('data', Filter, null);
+
+log("Fin de la politica NetcoolEventDelete");  
+  
